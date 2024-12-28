f54cb054ab23bee4553d3a9f4bb2056092afa013
daveagill <daveagill@users.noreply.github.com>
2024-12-28 14:27:21 +0000

    fix test

--- day22/test.ts
+++ day22/test.ts
@@ -3,7 +3,7 @@ import { part1 } from "./part1.ts";
 import { part2 } from "./part2.ts";
 
 Deno.test("part1", () => {
-    assertEquals(part1(), 20332089158);
+    assertEquals(part1(), 20332089158n);
 });
 
 Deno.test("part2", () => {
